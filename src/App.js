9f17da098d881adad7cd8f1994fabb43288fa279
Kennaann <kenanntap@gmail.com>
2022-02-24 12:52:11 +0100

    Dernière version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import {Routes, Route} from 'react-router-dom'
+import Coins from './components/Coins'
+import Coin from './routes/Coin'
+import Navbar from './components/Navbar'
 
+import './index.css'
 function App() {
+
+  // Hook pour le state qui va retourner un tableau de coins
+  const [coins, setCoins] = useState([]) // "[]" car l'Api retourne un tableau 
+
+  // url de ma requête vers l'API CoinGecko
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+
+  // remplace "componentDidMount" "componentWillMount"
+  useEffect(() => {
+    axios.get(url).then((response) => { // avec axios je récupère l'url qui va me renvoyer dans la réponse un tableau avec mes données
+      setCoins(response.data)
+    }).catch((error) => {  // si il y a des erreurs je les affiches
+      console.log(error) 
+    })
+  }, []) // "[]" pour pas que l'api fasse des appels en continue 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Coins coins={coins} />} />
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
